3b9093c209d7fef4877129f52e08c667a924e95f
Jack253-png <guoxiuchen20170402@163.com>
2023-01-12 17:40:17 +0800

    updated on 2023.1.11

--- src/main/java/com/mcreater/canimation/client/CAnimationClient.java
+++ src/main/java/com/mcreater/canimation/client/CAnimationClient.java
@@ -14,26 +14,7 @@ import java.security.cert.X509Certificate;
 @Environment(EnvType.CLIENT)
 public class CAnimationClient implements ClientModInitializer {
     public static final CommonConfig config = new CommonConfig();
-
     public void onInitializeClient() {
-        trustAllHosts();
 
-    }
-    public static void trustAllHosts() {
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                return new X509Certificate[]{};
-            }
-            public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-            public void checkServerTrusted(X509Certificate[] chain, String authType) {}
-        }
-        };
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
