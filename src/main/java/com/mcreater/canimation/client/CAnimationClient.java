aefa6a5e61e4274eab1684134eb174653c894db7
Jack253-png <guoxiuchen20170402@163.com>
2023-01-12 17:28:52 +0800

    updated on 2023.1.12

--- src/main/java/com/mcreater/canimation/client/CAnimationClient.java
+++ src/main/java/com/mcreater/canimation/client/CAnimationClient.java
@@ -1,36 +1,15 @@
 package com.mcreater.canimation.client;
 
-import com.google.gson.Gson;
 import com.mcreater.canimation.config.CommonConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.impl.FabricLoaderImpl;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.BossBarHud;
-import net.minecraft.client.gui.hud.ClientBossBar;
-import net.minecraft.entity.boss.BossBar;
-import net.minecraft.text.LiteralText;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Field;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.security.cert.X509Certificate;
-import java.util.Map;
-import java.util.UUID;
-import java.util.Vector;
-import java.util.function.Consumer;
-
-import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
 
 @Environment(EnvType.CLIENT)
 public class CAnimationClient implements ClientModInitializer {
@@ -38,64 +17,6 @@ public class CAnimationClient implements ClientModInitializer {
 
     public void onInitializeClient() {
         trustAllHosts();
-
-        if (FabricLoaderImpl.INSTANCE.isDevelopmentEnvironment()) {
-            ClientCommandManager.DISPATCHER.register(
-                    literal("//debugbar")
-                            .executes(context -> {
-                                UUID uuid = UUID.randomUUID();
-                                BossBarHud hud = MinecraftClient.getInstance().inGameHud.getBossBarHud();
-
-                                try {
-                                    Field f = BossBarHud.class.getDeclaredField("bossBars");
-                                    f.setAccessible(true);
-                                    Map<UUID, ClientBossBar> map = (Map<UUID, ClientBossBar>) f.get(hud);
-
-                                    map.put(uuid, new ClientBossBar(uuid, new LiteralText("debug"), 0, BossBar.Color.BLUE, BossBar.Style.NOTCHED_20, false, false, false));
-                                } catch (Exception e) {
-                                    throw new RuntimeException(e);
-                                }
-
-                                return 1;
-                            })
-            );
-
-            ClientCommandManager.DISPATCHER.register(
-                    literal("//cerbar")
-                            .executes(context -> {
-                                BossBarHud hud = MinecraftClient.getInstance().inGameHud.getBossBarHud();
-
-                                try {
-                                    Field f = BossBarHud.class.getDeclaredField("bossBars");
-                                    f.setAccessible(true);
-                                    Map<UUID, ClientBossBar> map = (Map<UUID, ClientBossBar>) f.get(hud);
-                                    map.clear();
-                                } catch (Exception e) {
-                                    throw new RuntimeException(e);
-                                }
-
-                                return 1;
-                            })
-            );
-
-            ClientCommandManager.DISPATCHER.register(
-                    literal("//rmfbar")
-                            .executes(context -> {
-                                BossBarHud hud = MinecraftClient.getInstance().inGameHud.getBossBarHud();
-
-                                try {
-                                    Field f = BossBarHud.class.getDeclaredField("bossBars");
-                                    f.setAccessible(true);
-                                    Map<UUID, ClientBossBar> map = (Map<UUID, ClientBossBar>) f.get(hud);
-                                    map.remove(map.keySet().iterator().next());
-                                } catch (Exception e) {
-                                    throw new RuntimeException(e);
-                                }
-
-                                return 1;
-                            })
-            );
-        }
     }
     public static void trustAllHosts() {
         TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
